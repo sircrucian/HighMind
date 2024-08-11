5d213dfa359834722c929b0dc070d392ff6af721
Roman Solentsov <romansolentsov@gmail.com>
2024-08-11 10:16:37 +0300

    Create new project

---
+++ Source/HighMind/HighMind.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HighMind.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HighMind, "HighMind" );
